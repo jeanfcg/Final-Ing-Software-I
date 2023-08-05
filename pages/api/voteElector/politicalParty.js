19b6191ddee54d87363b7b2d7784e60be3ff922b
Avelino Lupo <103952759+lino62U@users.noreply.github.com>
2023-08-05 02:07:51 -0500

    Update politicalParty.js

--- pages/api/voteElector/politicalParty.js
+++ pages/api/voteElector/politicalParty.js
@@ -1,11 +1,18 @@
-import {pool} from "@/ldavis/data/config/db";
-import VoteElector from "@/ldavis/services/VoteElector";
-export default async function handlePartidoPolitico(req,res){
-    try{
-        const result = await VoteElector.getPoliticalParty();
-        return res.status(200).json(result);
-    }
-    catch (err){
-        res.status(500).json({error: err})
-    }
+// Importar los módulos
+import { pool } from "../../data/config/db";
+import VoteElector from "../../services/VoteElector";
+
+// Utilizamos async/await para manejar promesas y manejamos los errores adecuadamente
+export default async function handlePartidoPolitico(req, res) {
+  try {
+    // Obtenemos los datos del partido político desde el servicio VoteElector
+    const datosPartidoPolitico = await VoteElector.getPoliticalParty();
+
+    // Devolvemos los datos en la respuesta con estado 200 (éxito)
+    return res.status(200).json(datosPartidoPolitico);
+  } catch (err) {
+    // Manejamos los errores y proporcionamos un mensaje de error claro
+    console.error("Error al obtener los datos del partido político:", err);
+    res.status(500).json({ error: "Error interno del servidor" });
+  }
 }
