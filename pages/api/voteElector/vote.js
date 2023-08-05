e1453b8a81403473671d7410d4db4c31c17d92df
Avelino Lupo <103952759+lino62U@users.noreply.github.com>
2023-08-05 02:12:16 -0500

    Update vote.js

--- pages/api/voteElector/vote.js
+++ pages/api/voteElector/vote.js
@@ -1,20 +1,25 @@
-import {pool} from "@/ldavis/data/config/db";
-import VoteElector from "@/ldavis/services/VoteElector";
-export default async function handleVote(req,res){
-    const vote = req.body;
+import { pool } from "../../data/config/db";
+import VoteElector from "../../services/VoteElector";
 
-    try{
-        const result = await VoteElector.sendVote(vote);
-        if(result.status === 200){
-            return res.status(200).json({message: "Voto correcto"})
-        }
-        else if(result.status === 401){
-            return res.status(401).json({message: "Usuario ya voto"})
-        }
-        console.log(result)
-        return res.status(500).json({message: "Ocurrio un error"})
+// Función para manejar una solicitud de voto
+export default async function handleVote(req, res) {
+  const vote = req.body; // Datos del voto desde el cuerpo de la solicitud
+
+  try {
+    // Intentamos enviar el voto al servicio VoteElector para su procesamiento
+    const result = await VoteElector.sendVote(vote);
+
+    // Verificamos el estado de la respuesta para dar la respuesta adecuada
+    if (result.status === 200) {
+      return res.status(200).json({ message: "Voto correcto" });
+    } else if (result.status === 401) {
+      return res.status(401).json({ message: "Usuario ya votó" });
     }
-    catch (err){
-        res.status(500).json({error: err})
-    }
+
+    // Si no se cumplieron las condiciones anteriores, respondemos con un error 500
+    return res.status(500).json({ message: "Ocurrió un error" });
+  } catch (err) {
+    // Si ocurre algún error durante el proceso, lo capturamos y respondemos con un error 500
+    res.status(500).json({ error: err }); 
+  }
 }
