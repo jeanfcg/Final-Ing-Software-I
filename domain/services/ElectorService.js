288dd1187024b8e3619e11ab54e265e8a36ff977
Avelino Lupo <103952759+lino62U@users.noreply.github.com>
2023-08-05 02:23:40 -0500

    Update ElectorService.js

--- domain/services/ElectorService.js
+++ domain/services/ElectorService.js
@@ -1,21 +1,29 @@
-import ElectorRepository from "@/ldavis/data/repository/ElectorRepository";
-import Vote from "@/ldavis/domain/models/Vote";
+import ElectorRepository from "../../data/repository/ElectorRepository"; // Ruta relativa para ElectorRepository
+import Vote from "../../domain/models/Vote"; // Ruta relativa para Vote model
+
 class ElectorService {
-    static async saveVote(vote) {
-        const instanceVote = new Vote(vote.idElector,vote.idPoliticalParty,vote.date)
-        try{
-            return await ElectorRepository.saveVote(instanceVote);
-        } catch (error){
-            return error;
-        }
+  // Función para guardar un voto
+  static async saveVote(vote) {
+    // Creamos una instancia del modelo Vote con los datos recibidos
+    const instanceVote = new Vote(vote.idElector, vote.idPoliticalParty, vote.date);
+
+    try {
+      // Llamada al repositorio ElectorRepository para guardar el voto
+      return await ElectorRepository.saveVote(instanceVote);
+    } catch (error) {
+      return error; // Devuelve el error en caso de fallo en la llamada al repositorio
     }
-    static async getPoliticalParty(){
-        try{
-            return await ElectorRepository.getPoliticalParty();
-        } catch (error){
-            return error;
-        }
+  }
+
+  // Función para obtener los partidos políticos
+  static async getPoliticalParty() {
+    try {
+      // Llamada al repositorio ElectorRepository para obtener los partidos políticos
+      return await ElectorRepository.getPoliticalParty();
+    } catch (error) {
+      return error; // Devuelve el error en caso de fallo en la llamada al repositorio
     }
+  }
 }
 
 export default ElectorService;
