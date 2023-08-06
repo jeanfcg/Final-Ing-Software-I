6922ec63045e1439850af534835042772567e266
ALdoMartineCh16 <86008861+ALdoMartineCh16@users.noreply.github.com>
2023-08-05 19:18:09 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,90 @@
-export default function IndexPage() {
+import Link from 'next/link';
+import React, { useEffect, useState } from 'react';
+import Head from 'next/head';
+import axios from 'axios';
+
+// Single Responsibility Principle: Create a CandidateRow component
+const CandidateRow = ({ candidate }) => {
+  const { nombre, apellido, cargo, nombre_partido } = candidate;
   return (
-      <>
-        <div>Home</div>
-      </>
+    <tr>
+      <td>{nombre}</td>
+      <td>{apellido}</td>
+      <td>{cargo}</td>
+      <td>{nombre_partido}</td>
+      <td>
+        <img
+          src={`/partidos/${nombre_partido}.png`}
+          alt={'Cargando'}
+          style={{ width: '100px', height: '100px' }}
+        />
+      </td>
+    </tr>
+  );
+};
+
+// Open/Closed Principle: The Table component can be extended and customized with props.
+const Table = ({ data }) => {
+  return (
+    <table className="table mt-4">
+      <thead>
+        <tr>
+          <th>Nombre</th>
+          <th>Apellido</th>
+          <th>Cargo</th>
+          <th>Partido</th>
+          <th>Logo</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((candidate, index) => (
+          <CandidateRow key={index} candidate={candidate} />
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default function IndexPage() {
+  const [candidatos, setCandidatos] = useState([]);
+  const [page, setPage] = useState(1);
+  const pageSize = 10;
+
+  useEffect(() => {
+    getCandidatos();
+  }, [page]);
+
+  const getCandidatos = async () => {
+    try {
+      const res = await axios.get(`/api/candidatos/?page=${page}&pageSize=${pageSize}`);
+      const data = res.data[0].sort((a, b) => a.nombre_partido.localeCompare(b.nombre_partido));
+      setCandidatos((prevCandidatos) => [...prevCandidatos, ...data]);
+    } catch (error) {
+      console.error("Error fetching candidates:", error);
+      // TODO: Handle the error state, show an error message, or retry the API call.
+    }
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <div className="container mt-5">
+        <div>Bienvenido</div>
+        {/* SOLID - Conditional Rendering */}
+        {candidatos.length > 0 ? (
+          <Table data={candidatos.filter((c) => c.nombre_partido !== 'Nulo' && c.cargo === 'Presidente')} />
+        ) : (
+          <div>Vacio</div>
+        )}
+        <div className="mt-3">
+          <Link href="/login">Iniciar sesion</Link>
+        </div>
+        <div>
+          <Link href="/new">Registrarse</Link>
+        </div>
+      </div>
+    </>
   );
 }
